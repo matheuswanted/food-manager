ccc33d07fbc4813f989c7492fadc94529646bc64
Matheus Santos <matheuswantedac@live.com>
2020-11-20 12:52:37 -0300

    add tests and mongo

---
+++ test/stock/Wanted.FoodManager.Stock.IT/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Wanted.FoodManager.Stock.IT
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ test/stock/Wanted.FoodManager.Stock.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Wanted.FoodManager.Stock.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
